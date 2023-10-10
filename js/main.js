8240b5ecc17354c39b5b71c3a5a63bcd1d22b4b0
Ibrahim Ahmed <ibrahimahmed90000@gmail.com>
2023-10-10 19:50:42 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// Scrolling from linke in NavBar
+let navbar = document.querySelector(".navbar");
+let navbarLinks = document.querySelectorAll(".navbar .navbar-nav .nav-link");
+
+document.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    navbar.classList.add("shadow");
+  } else {
+    navbar.classList.remove("shadow");
+  }
+});
+
+function removeActiveLink() {
+  navbarLinks.forEach((link) => {
+    link.classList.remove("active");
+  });
+}
+
+navbarLinks.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    removeActiveLink();
+    e.target.classList.add("active");
+  });
+});
+
+// ===============================================================
+// For sending me email with subject and Message
+
+let mailSubject = document.getElementById("mail-subject");
+let mailMessage = document.getElementById("mail-msg");
+let sendEmail = document.getElementById("email-me");
+
+sendEmail.addEventListener("click", function (e) {
+  e.target.href += `?subject=${mailSubject.value}&body=${mailMessage.value}.`;
+});
+// =====================================================
+// Scroll To Top Button
+const scrollToTop = document.querySelector(".rowup");
+window.addEventListener("scroll", () => {
+  const scrollPos = window.scrollY;
+  const viewportHeight = window.innerHeight;
+
+  if (scrollPos > viewportHeight / 2) {
+    scrollToTop.classList.add("visible");
+  } else {
+    scrollToTop.classList.remove("visible");
+  }
+});
